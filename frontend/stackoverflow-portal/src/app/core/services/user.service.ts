f3695afcf1d4ecea8a3a1e35321fbe2289e8861b
ivamladenovic <mladenoviciva11@gmail.com>
2025-09-16 14:55:49 +0200

    Implemented Registration UI

---
+++ frontend/stackoverflow-portal/src/app/core/services/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../env/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly httpClient: HttpClient = inject(HttpClient);
+  private readonly usersUrl = `${environment.apiUrl}/users`;
+
+  uploadPhoto(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.httpClient.post(`${this.usersUrl}/photo`, formData);
+  }
+}
