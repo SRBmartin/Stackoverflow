c42f20b1d176e74685e5d32e1fa4f6a5028db6e1
Martin.Muharemovic <makimuh02@gmail.com>
2025-09-20 16:40:50 +0200

    Implemented mail service.

---
+++ backend/mail-service/project/Application/Features/Commands/SendMailCommand.cs
@@ -0,0 +1,25 @@
+﻿using MailService.Application.Contracts.Services;
+using MediatR;
+
+namespace MailService.Application.Features.Commands;
+
+public class SendMailCommandRequest : IRequest<bool>
+{
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
+
+public class SendMailCommandRequestHandler : IRequestHandler<SendMailCommandRequest, bool>
+{
+    private readonly IMailSenderService _mailSenderService;
+
+    public SendMailCommandRequestHandler(IMailSenderService mailSenderService)
+    {
+        _mailSenderService = mailSenderService;
+    }
+    public async Task<bool> Handle(SendMailCommandRequest request, CancellationToken cancellationToken = default)
+    {
+        return await _mailSenderService.SendMailAsync(request.To, request.Subject, request.Body);
+    }
+}
---
+++ backend/mail-service/project/Infrastructure/Services/MailSenderService.cs
@@ -0,0 +1,41 @@
+﻿using MailService.Application.Contracts.Services;
+using MailService.Infrastructure.Services.Configuration;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace MailService.Infrastructure.Services;
+
+public class MailSenderService : IMailSenderService
+{
+    private readonly MailSettings _settings;
+
+    public MailSenderService(MailSettings settings)
+    {
+        _settings = settings;
+    }
+
+    public async Task<bool> SendMailAsync(string to, string subject, string body)
+    {
+        try
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(_settings.SenderName, _settings.SenderEmail));
+            message.To.Add(new MailboxAddress("", to));
+            message.Subject = subject;
+            message.Body = new TextPart("html") { Text = body };
+
+            using var client = new SmtpClient();
+            
+            await client.ConnectAsync(_settings.SmtpServer, _settings.Port, true);
+            await client.AuthenticateAsync(_settings.Username, _settings.Password);
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error sending email: {ex.Message}");
+            return false;
+        }
+    }
+}
