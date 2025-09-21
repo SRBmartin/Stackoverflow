5488587f26928d0a15638ce4377515ebc8b56458
Martin.Muharemovic <makimuh02@gmail.com>
2025-09-21 14:57:53 +0200

    Implemented /health-monitoring on NotificationService and Stackoverflow.WebRole services.

---
+++ backend/Stackoverflow/src/StackOverflowService/StackOverflowService.WebRole/Controllers/HealthMonitoringController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace StackOverflowService.WebRole.Controllers
+{
+    [RoutePrefix("")]
+    public class HealthMonitoringController : ApiController
+    {
+        [HttpGet, Route("health-monitoring")]
+        [AllowAnonymous]
+        public IHttpActionResult HealthStatus()
+        {
+            return Ok();
+        }
+
+    }
+}
